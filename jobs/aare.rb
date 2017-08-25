e591f0d0208e62dbce85755bd66104501fe39c0a
Oliver Gugger <gugger@puzzle.ch>
2017-08-25 12:01:36 +0200

    Use open source API api.illubits.ch

--- jobs/aare.rb
+++ jobs/aare.rb
@@ -2,13 +2,20 @@
 require 'open-uri'
 require 'json'
 
-SCHEDULER.every '30m', :first_in => 0 do |job|
-  url = URI.parse('http://aare.schwumm.ch/aare.json')
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  # code for API is at https://github.com/guggero/aare-server
+  url = URI.parse('https://api.illubits.ch/aare/v1/current')
   req = Net::HTTP::Get.new(url.to_s)
-  res = Net::HTTP.start(url.host, url.port) {|http|
+  res = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') {|http|
     http.request(req)
   }
   parsed  = JSON.parse(res.body)
-  temperature = parsed["temperature"]
+  temperature = "0.0"
+  parsed["stations"].each{|station|
+    if "Aare - Bern".eql? station["name"]
+      temperature = station["temperature"]
+    end
+  }
   send_event('aare', { temperature: temperature })
 end
+
